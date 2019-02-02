7692bc52ac416eafe9ccf5713067e1137a060c03
evansoohoo <evan.soohoo@gmail.com>
2019-02-02 09:51:58 -0800

    stripping this shiz down with the intention of going from kotlin room to just straight sqlite, which i think will be easier to debug. keeping dependencies laying around in build.gradle, but more or less trying to move away from room

--- app/src/main/java/com/first/evansoohoo/carbcounter/MainActivity.kt
+++ app/src/main/java/com/first/evansoohoo/carbcounter/MainActivity.kt
@@ -7,83 +7,12 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.PrimaryKey
-import android.arch.persistence.room.Dao
-import android.arch.persistence.room.Query
-import android.arch.persistence.room.Insert
-import android.arch.persistence.room.Delete
-import android.arch.persistence.room.Database
-import android.arch.persistence.room.Room
-import android.arch.persistence.room.RoomDatabase
-import android.content.Context
 
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import android.content.Context
 
 class MainActivity : AppCompatActivity() {
 
-    @Entity
-    data class User(
-            @PrimaryKey var uid: Int,
-            @ColumnInfo(name = "carb_count") var carbCount: Int?,
-            @ColumnInfo(name = "last_name") var lastName: String?
-    )
 
-    @Dao
-    interface UserDao {
-
-        @Insert()
-        fun insertUser(user: User )
-
-        @Query("SELECT * FROM user")
-        fun getAll(): List<User>
-
-        @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-        fun loadAllByIds(userIds: IntArray): List<User>
-
-        //@Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-        //        "last_name LIKE :last LIMIT 1")
-        //fun findByName(first: String, last: String): User
-
-        //@Insert
-        //fun insertAll(vararg users: User)
-
-        @Delete
-        fun delete(user: User)
-    }
-
-    @Database(entities = arrayOf(User::class), version = 1)
-    abstract class AppDatabase : RoomDatabase() {
-
-        abstract fun userDao(): UserDao
-
-        companion object {
-
-            /**
-             * The only instance
-             */
-            private var sInstance: AppDatabase? = null
-
-            /**
-             * Gets the singleton instance of SampleDatabase.
-             *
-             * @param context The context.
-             * @return The singleton instance of SampleDatabase.
-             */
-            @Synchronized
-            fun getInstance(context: Context): AppDatabase {
-                if (sInstance == null) {
-                    sInstance = Room
-                            .databaseBuilder(context.applicationContext, AppDatabase::class.java, "example")
-                            .fallbackToDestructiveMigration()
-                            .build()
-                }
-                return sInstance!!
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         var sum:  Int = 0
@@ -97,37 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         button2.setOnClickListener {
             Log.d("TAG", "ES: You hit the save button")
-            val db = Room.databaseBuilder(
-                    applicationContext,
-                    AppDatabase::class.java, "database-name"
-            ).build()
 
-            Log.d("TAG", "ES: Defined user. using separate thread")
-            var thisUserDao = db?.userDao()
-            doAsync {
-                Log.d("TAG", "ES: ENTERED DOASYNC LOOP")
-                with(thisUserDao) {
-                    var user1 = User(uid=0, carbCount=sum, lastName="wu")
-
-                    Log.d("TAG", "ES: I ALSO WANT TO SEE THIS TO INSERT ENTRY (attempting insertUser below)")
-                    Log.d("TAG", "ES: Test to see if it ever gets past insertUser")
-                    //this?.insertUser(user1)
-                    AppDatabase.getInstance(this@MainActivity).userDao().insertUser(user1)
-                    Log.d("TAG", "ES: Just inserted entry with sum $sum")
-                    val allUserData = thisUserDao?.getAll() //ES: Error identified. It doesn't do anything for getAll
-                    Log.d("TAG", "ES: What if we just print it directly? size is $allUserData.size")
-                    val entries = allUserData.size
-                    Log.d("TAG", "ES: Now the number of entries is $entries")
-                    Log.d("TAG", "ES: The 0th value of alluserdata is $allUserData.get(0)")
-
-                    uiThread {
-                        val allUserData = thisUserDao?.getAll() //Error: It doesn't get here either
-                        val entries = allUserData.size
-                        Log.d("TAG", "ES: Now the number of entries is $entries")
-                    }
-
-                }
-            }
         }
     }
 
