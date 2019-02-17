2b57d7b878b296a1559742d6a5b1d8d8b3da000d
evansoohoo <evan.soohoo@gmail.com>
2019-02-17 08:44:05 -0800

    separated FeedReaderContract into a separate file and made fields private, but I'd like to learn more about Kotlin import statements and kotlin scope

---
+++ app/src/main/java/com/first/evansoohoo/carbcounter/FeedReaderContract.kt
@@ -0,0 +1,19 @@
+package com.first.evansoohoo.carbcounter
+
+import android.provider.BaseColumns
+
+object FeedReaderContract {
+    // Table contents are grouped together in an anonymous object.
+    object FeedEntry : BaseColumns {
+        const val TABLE_NAME = "allcarbentries"
+        const val COLUMN_NAME_TITLE = "carbs"
+        const val COLUMN_NAME_SUBTITLE = "subtitle" //idk what that means
+    }
+    const val SQL_CREATE_ENTRIES =
+            "CREATE TABLE ${FeedEntry.TABLE_NAME} (" +
+                    "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                    "${FeedEntry.COLUMN_NAME_TITLE} TEXT," +
+                    "${FeedEntry.COLUMN_NAME_SUBTITLE} TEXT)"
+
+    const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${FeedEntry.TABLE_NAME}"
+}
--- app/src/main/java/com/first/evansoohoo/carbcounter/MainActivity.kt
+++ app/src/main/java/com/first/evansoohoo/carbcounter/MainActivity.kt
@@ -6,13 +6,32 @@ import android.util.Log
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-
+import android.database.sqlite.*
 
 import android.content.Context
 
+class FeedReaderDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(FeedReaderContract.SQL_CREATE_ENTRIES)
+    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(FeedReaderContract.SQL_DELETE_ENTRIES)
+        onCreate(db)
+    }
+    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        onUpgrade(db, oldVersion, newVersion)
+    }
+    companion object {
+        // If you change the database schema, you must increment the database version.
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "FeedReader.db"
+    }
+}
+
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         var sum:  Int = 0
@@ -26,6 +45,7 @@ class MainActivity : AppCompatActivity() {
 
         button2.setOnClickListener {
             Log.d("TAG", "ES: You hit the save button")
+
 
         }
     }
