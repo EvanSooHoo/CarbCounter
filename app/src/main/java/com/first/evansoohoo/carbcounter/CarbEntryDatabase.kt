375e5ac1c161756e37cb5ac0c8d47c320334093e
evansoohoo <evan.soohoo@gmail.com>
2019-04-19 07:36:17 -0700

    Trying to use room again; split things up as mindorks did, into multiple kotlin files

---
+++ app/src/main/java/com/first/evansoohoo/carbcounter/CarbEntryDatabase.kt
@@ -0,0 +1,37 @@
+package com.first.evansoohoo.carbcounter
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+
+@Database(entities = arrayOf(User::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+
+    companion object {
+
+        /**
+         * The only instance
+         */
+        private var sInstance: AppDatabase? = null
+
+        /**
+         * Gets the singleton instance of SampleDatabase.
+         *
+         * @param context The context.
+         * @return The singleton instance of SampleDatabase.
+         */
+        @Synchronized
+        fun getInstance(context: Context): AppDatabase {
+            if (sInstance == null) {
+                sInstance = Room
+                        .databaseBuilder(context.applicationContext, AppDatabase::class.java, "example")
+                        .fallbackToDestructiveMigration()
+                        .build()
+            }
+            return sInstance!!
+        }
+    }
+}
--- app/src/main/java/com/first/evansoohoo/carbcounter/MainActivity.kt
+++ app/src/main/java/com/first/evansoohoo/carbcounter/MainActivity.kt
@@ -24,66 +24,6 @@ import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
-    @Entity
-    data class User(
-            @PrimaryKey var uid: Int,
-            @ColumnInfo(name = "carb_count") var carbCount: Int?,
-            @ColumnInfo(name = "last_name") var lastName: String?
-    )
-
-    @Dao
-    interface UserDao {
-
-        @Insert()
-        fun insertUser(user: User )
-
-        @Query("SELECT * FROM user")
-        fun getAll(): List<User>
-
-        @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-        fun loadAllByIds(userIds: IntArray): List<User>
-
-        //@Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-        //        "last_name LIKE :last LIMIT 1")
-        //fun findByName(first: String, last: String): User
-
-        //@Insert
-        //fun insertAll(vararg users: User)
-
-        @Delete
-        fun delete(user: User)
-    }
-
-    @Database(entities = arrayOf(User::class), version = 1)
-    abstract class AppDatabase : RoomDatabase() {
-
-        abstract fun userDao(): UserDao
-
-        companion object {
-
-            /**
-             * The only instance
-             */
-            private var sInstance: AppDatabase? = null
-
-            /**
-             * Gets the singleton instance of SampleDatabase.
-             *
-             * @param context The context.
-             * @return The singleton instance of SampleDatabase.
-             */
-            @Synchronized
-            fun getInstance(context: Context): AppDatabase {
-                if (sInstance == null) {
-                    sInstance = Room
-                            .databaseBuilder(context.applicationContext, AppDatabase::class.java, "example")
-                            .fallbackToDestructiveMigration()
-                            .build()
-                }
-                return sInstance!!
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         var sum:  Int = 0
