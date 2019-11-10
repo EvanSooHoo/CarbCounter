9d0c94ca0a2cd4763597d0059c21a1460e9a381b
evansoohoo <evan.soohoo@gmail.com>
2019-11-09 19:57:06 -0800

    added code for database insertion/access; not yet functioning properly

--- app/src/main/java/com/first/evansoohoo/carbcounter/MainActivity.kt
+++ app/src/main/java/com/first/evansoohoo/carbcounter/MainActivity.kt
@@ -15,10 +15,11 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-
+import androidx.room.Room
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         var sum:  Int = 0
@@ -31,12 +32,34 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,Integer.toString(sum),Toast.LENGTH_LONG).show()
         }
 
-
-/*
         button2.setOnClickListener {
             Log.d("TAG", "ES: You hit the save button")
+            val db = Room.databaseBuilder(
+                    applicationContext,
+                    CarbRoomDatabase::class.java, "CarbEntry.db"
+            ).build()
+
+            GlobalScope.launch {
+                db.CarbEntry().insert(CarbEntry(sum))
+
+            }
+            Log.d("TAG", "ES: Attempted to save sum " + sum + " into database")
         }
-        */
+
+        button3.setOnClickListener {
+            Log.d("TAG", "ES: You hit the previous entries list")
+            val db = Room.databaseBuilder(
+                    applicationContext,
+                    CarbRoomDatabase::class.java, "CarbEntry.db"
+            ).build()
+
+            var data = db.CarbEntry().getAllCarbEntries()
+            Log.d("TAG", "ES: Size of carb entries database is now " + (data.value?.size))
+
+
+            Log.d("TAG", "ES: Value of saved entry at end is " + (data.getValue()?.get(0)))
+        }
+
 
     }
 
